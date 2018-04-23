5fac62f3cb6bd33a13d9f1cb587dad690b6bbd4d
DHSM <mostafizrbn@gmail.com>
2018-04-23 12:46:27 +0600

    1st Commit

---
+++ src/main/java/controller/StudentController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import data.model.Student;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="StudentController")
+public class StudentController {
+    private Student student = new Student();
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public String isNextPage() {
+        return "viewstudent";
+    }
+}
---
+++ src/main/java/data/model/Student.java
@@ -0,0 +1,25 @@
+package data.model;
+
+public class Student {
+    private Integer roll;
+    private  String name;
+
+    public Student() {
+    }
+
+    public Integer getRoll() {
+        return roll;
+    }
+
+    public void setRoll(Integer roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
